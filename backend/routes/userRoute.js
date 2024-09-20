8b6f661a89bf1d1086967fb32e831a93326a359e
babulalmahto <babulalmatho4394@gmail.com>
2024-09-20 19:33:11 +0530

    User register & user login api created

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,10 @@
+//user login and signup
+import express from 'express'
+import { loginUser, registerUser } from '../controllers/userController.js'
+
+const userRouter = express.Router()
+
+userRouter.post('/register', registerUser)
+userRouter.post('/login', loginUser)
+
+export default userRouter
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 import { connectdb } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import 'dotenv/config';
 
 // app config
 const app = express();
@@ -8,6 +11,7 @@ const port = 4000;
 
 //middleware
 app.use(express.json());
+app.use(cors())
 
 // Db Connection
 connectdb()
@@ -15,6 +19,7 @@ connectdb()
 // api endpoints
 app.use('/api/food', foodRouter);
 app.use("/images", express.static('uploads'))
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
     res.send('API Working');
