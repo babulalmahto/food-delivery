a943f41f623795b41043e84f25fac4a4e5fdeeef
babulalmahto <babulalmatho4394@gmail.com>
2024-09-22 01:11:09 +0530

    payment & verification process done in backend

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,78 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.SRTIPE_SECRET_KEY)
+
+
+// Placing user order for frontend
+const placeOrder = async (req, res) => {
+
+    const frontend_url = "http://127.0.0.1:5173"
+
+    try {
+        const newOrder = new orderModel({
+            userId: req.body.userId,
+            items: req.body.items,
+            amount: req.body.amount,
+            address: req.body.address
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+
+        const line_items = req.body.items.map((item) => ({
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: item.name
+                },
+                unit_amount: item.price * 100  //INR=item.price*100*80
+            },
+            quantity: item.quantity
+        }))
+
+        line_items.push({
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: "Delivery Charges"
+                },
+                unit_amount: 2 * 100    //INR=2*100*80
+            },
+            quantity: 1
+        })
+
+        console.log("######");
+        const session = await stripe.checkout.sessions.create({
+            line_items: line_items,
+            mode: 'payment',
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+        })
+
+        res.json({ success: true, session_url: session.url })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+    }
+}
+
+const verifyOrder = async (req, res) => {
+    const { orderId, success } = req.body;
+    try {
+        if (success == "true") {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            res.json({ success: true, message: "Paid" })
+        } else {
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({ success: false, message: "Not Paid" })
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Eror" })
+    }
+}
+
+
+export { placeOrder, verifyOrder, userOrders }
