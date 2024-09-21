71ee9983ef2d194dd79cb0a25020a276e7d083d6
babulalmahto <babulalmatho4394@gmail.com>
2024-09-21 13:49:04 +0530

    Created Api - addToCart, removeFromCart & getCart

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectdb } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config';
+import cartRouter from './routes/cartRoute.js';
 
 // app config
 const app = express();
@@ -19,7 +20,8 @@ connectdb()
 // api endpoints
 app.use('/api/food', foodRouter);
 app.use("/images", express.static('uploads'))
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter)
 
 app.get('/', (req, res) => {
     res.send('API Working');
