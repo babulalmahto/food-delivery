2093e4baf9308272e1fd1c4d5e007812fdc2476a
babulalmahto <babulalmatho4394@gmail.com>
2024-09-22 01:19:30 +0530

    Created API for user orders

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -6,6 +6,7 @@ const orderRouter = express.Router();
 
 orderRouter.post('/place', authMiddleware, placeOrder);;
 orderRouter.post('/verify', verifyOrder);
+orderRouter.post('/userorders', authMiddleware, userOrders)
 
 
 export default orderRouter
