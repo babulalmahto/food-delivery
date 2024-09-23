d8c5b09a224d8872aad21047a42ce8c78b0f0881
babulalmahto <babulalmatho4394@gmail.com>
2024-09-24 01:59:16 +0530

    Created order update feature in admin panel

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -20,6 +20,17 @@ const Orders = ({ url }) => {
         }
     }
 
+    const statusHandler = async (event, orderId) => {
+        console.log(event, orderId) //geting
+        const response = await axios.post(url + "/api/order/status", {
+            orderId,
+            status: event.target.value
+        })
+        if (response.data.success) {
+            await fetchAllOrders();
+        }
+    }
+
     useEffect(() => {
         fetchAllOrders()
     }, [])
@@ -50,7 +61,7 @@ const Orders = ({ url }) => {
                         </div>
                         <p>Items : {order.items.length}</p>
                         <p>${order.amount}</p>
-                        <select>
+                        <select onChange={(event) => statusHandler(event, order._id)} value={order.status}>
                             <option value="Food Processing">Food Processing</option>
                             <option value="Out For Delivery">Out For Delivery</option>
                             <option value="Deliverd">Deliverd</option>
